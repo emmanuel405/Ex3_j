d07eb32650a053b2d18f881f244f15eb8cc60c8c
emmanuel405 <57897803+emmanuel405@users.noreply.github.com>
2020-01-16 09:27:18 +0200

    Update Robot.java

--- src/gameClient/Robot.java
+++ src/gameClient/Robot.java
@@ -1,20 +1,19 @@
 package gameClient;
 
-
 import java.util.List;
 
 import dataStructure.node_data;
 import utils.Point3D;
 
 public class Robot {
-	public  int id;
-	public int src;
-	public int dest;
+	public  int id, src, dest;
 	public int speed=1;
 	public int value=0; 
 	public Point3D pos;
 	public List<node_data> path;
+	
 	int num=0;
+
 	public Robot() {
 		this. value=0;
 		this. id=num++;
@@ -22,21 +21,21 @@ public class Robot {
 		this.dest=-1;
 		this.pos=Point3D.ORIGIN;
 	}
-	
-public Robot(int id,int value,int src,int dest ,int speed,String pos) {
-	this. value=value;
-	this. id=id;
-	this.src=src;
-	this.dest=dest;
-	this.speed=speed;
-	
-    String[] cord= pos.split(",");
-	Point3D ans=new Point3D(Double.parseDouble(cord[0]),Double.parseDouble(cord[1]));
-	this. pos=ans;
-}
 
-public Point3D getLocation() {
-	return this.pos;
+	public Robot(int id,int value,int src,int dest ,int speed,String pos) {
+		this. value=value;
+		this. id=id;
+		this.src=src;
+		this.dest=dest;
+		this.speed=speed;
 
-}
+		String[] cord= pos.split(",");
+		Point3D ans=new Point3D(Double.parseDouble(cord[0]),Double.parseDouble(cord[1]));
+		this. pos=ans;
+	}
+	
+	public Point3D getLocation() {
+		return this.pos;
+
+	}
 }
