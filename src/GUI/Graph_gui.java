2831bcf6381689ef5af02f42ef564bd8e51bf477
emmanuel405 <57897803+emmanuel405@users.noreply.github.com>
2020-01-17 14:20:18 +0200

    Update Graph_gui.java

--- src/GUI/Graph_gui.java
+++ src/GUI/Graph_gui.java
@@ -1,6 +1,5 @@
 package GUI;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Menu;
@@ -20,10 +19,11 @@ import utils.Point3D;
 import java.util.LinkedList;
 
 public class Graph_gui extends JFrame implements ActionListener, MouseListener, MouseMotionListener{
+	Thread t;
 	public DGraph dg = null;
 	Graph_Algo g_a = new Graph_Algo(dg);
 	Point3D point_pressed = null;
-	public LinkedList<node_data> list = new LinkedList<node_data>();
+	public LinkedList<node_data> list_of_press = new LinkedList<node_data>();
 	LinkedList<Point3D> node_loc = new LinkedList<Point3D>();
 
 	private int BIGGER = 5;
@@ -41,11 +41,11 @@ public class Graph_gui extends JFrame implements ActionListener, MouseListener, 
 		mBar.add(m);
 		this.setMenuBar(mBar);
 
-		MenuItem item = new MenuItem("yadany");
+		MenuItem item = new MenuItem("Manual");
 		item.addActionListener(this);
-		MenuItem item1 = new MenuItem("otomat");
+		MenuItem item1 = new MenuItem("Automatic");
 		item1.addActionListener(this);
-		MenuItem item2 = new MenuItem("senario");
+		MenuItem item2 = new MenuItem("Senario");
 		item2.addActionListener(this);
 
 		m.add(item);
@@ -58,45 +58,51 @@ public class Graph_gui extends JFrame implements ActionListener, MouseListener, 
 	public void paint(Graphics g) {
 		super.paint(g);
 		g.setColor(Color.BLACK);
-		g.drawString("בס''ד", 950, 70);    	
+		g.drawString("בס''ד", 950, 70);
 
 		if (null == dg) return;
 
-		for (node_data n : dg.Vertex) {
-			node_loc.add(n.getLocation()); // Take a location of all nodes in the graph
+		try {
+			for (node_data n : dg.Vertex) {
+				node_loc.add(n.getLocation()); // Take a location of all nodes in the graph
 
-			g.setColor(Color.GREEN);
-			g.fillOval((int)n.getLocation().x() - BIGGER, (int)n.getLocation().y() - BIGGER,
-					10, 10);
+				g.setColor(Color.GREEN);
+				g.fillOval((int)n.getLocation().x() - BIGGER, (int)n.getLocation().y() - BIGGER,
+						10, 10);
 
-			NodeData nn = (NodeData)n;
-			for (NodeData m :nn.outgoing ) {
-				g.setColor(Color.RED);
-				g.drawLine(n.getLocation().ix(), n.getLocation().iy(),
-						m.getLocation().ix(), m.getLocation().iy());
+				NodeData nn = (NodeData)n;
+				for (NodeData m :nn.outgoing ) {
+					g.setColor(Color.RED);
+					g.drawLine(n.getLocation().ix(), n.getLocation().iy(),
+							m.getLocation().ix(), m.getLocation().iy());
 
-				edge_data ed = dg.getEdge(n.getKey(), m.getKey());
-				g.drawString(String.format("%.2f", ed.getWeight()),
-						drawOnLine(n.getLocation().x(), m.getLocation().x(), 0.75),
-						drawOnLine(n.getLocation().y(), m.getLocation().y(), 0.75));
+					edge_data ed = dg.getEdge(n.getKey(), m.getKey());
+					g.drawString(String.format("%.2f", ed.getWeight()),
+							drawOnLine(n.getLocation().x(), m.getLocation().x(), 0.75),
+							drawOnLine(n.getLocation().y(), m.getLocation().y(), 0.75));
 
-				g.setColor(Color.BLACK);
-				g.fillOval(drawOnLine(n.getLocation().x(), m.getLocation().x(), 0.85),
-						drawOnLine(n.getLocation().y(), m.getLocation().y(), 0.85), 4, 4);
+					g.setColor(Color.BLACK);
+					g.fillOval(drawOnLine(n.getLocation().x(), m.getLocation().x(), 0.85),
+							drawOnLine(n.getLocation().y(), m.getLocation().y(), 0.85), 4, 4);
+				}
 			}
-		}
-		//////////////append robots
-		for (Robot n : dg.Robots) {
-			g.setColor(Color.blue);
-			g.fillOval(n.getLocation().ix() - BIGGER, n.getLocation().iy() - BIGGER,
-					(int)2.5*BIGGER, (int)2.5*BIGGER);
-		}
+			//////////////append robots
+			for (Robot n : dg.Robots) {
+				g.setColor(Color.blue);
+				g.fillOval(n.getLocation().ix() - BIGGER, n.getLocation().iy() - BIGGER,
+						(int)2.5*BIGGER, (int)2.5*BIGGER);
+			}
 
-		//////////////append fruits
-		for (Fruit n : dg.Fruits) {
-			g.setColor(Color.orange);
-			g.fillOval(n.getLocation().ix() - BIGGER, n.getLocation().iy() - BIGGER,
-					(int)2.5*BIGGER, (int)2.5*BIGGER);
+			//////////////append fruits
+			for (Fruit n : dg.Fruits) {
+				g.setColor(Color.orange);
+				g.fillOval(n.getLocation().ix() - BIGGER, n.getLocation().iy() - BIGGER,
+						(int)2.5*BIGGER, (int)2.5*BIGGER);
+				
+			}
+			t.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -109,25 +115,31 @@ public class Graph_gui extends JFrame implements ActionListener, MouseListener, 
 
 		switch(s) {
 
-		case "Load":
-
+		case "Manual":
+			
+			repaint();
 			break;
 
-		case "Save":
-
+		case "Automatic":
+			
+			repaint();
 			break;
+
+		case "Senario":
+			
+			repaint();
+			break;
+
 		}
 
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent m_e) {
-
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent m_e) {
-
 	}
 
 	@Override
@@ -151,7 +163,7 @@ public class Graph_gui extends JFrame implements ActionListener, MouseListener, 
 		}
 		for (node_data nd : dg.Vertex) {
 			if(point_pressed == nd.getLocation()) {
-				list.add(nd);
+				list_of_press.add(nd);
 				System.out.println(nd.getKey());
 			}
 		}
