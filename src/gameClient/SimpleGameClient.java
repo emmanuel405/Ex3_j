411bc5cc40c9801e3ddce40fa0f53831f83dba99
emmanuel405 <57897803+emmanuel405@users.noreply.github.com>
2020-01-17 14:10:05 +0200

    Update SimpleGameClient.java

--- src/gameClient/SimpleGameClient.java
+++ src/gameClient/SimpleGameClient.java
@@ -1,7 +1,5 @@
 package gameClient;
 
-
-
 import java.util.Collection;
 
 import java.util.Iterator;
@@ -10,13 +8,9 @@ import java.util.LinkedList;
 
 import java.util.List;
 
-
-
 import org.json.JSONException;
 
 import org.json.JSONObject;
-
-
 
 import GUI.Graph_gui;
 
@@ -75,24 +69,19 @@ public class SimpleGameClient {
 	static Graph_gui gu = new Graph_gui();
 
 	public static void main(String[] a) throws JSONException {
-
-		test1();}
-
+		test1();
+	}
 	public static void test1() throws JSONException {
 
-
-
-		int scenario_num =5;
+		int scenario_num = 5;
 
 		game_service game = Game_Server.getServer(scenario_num); // you have [0,23] games
 
 		String g = game.getGraph();
 
 		gg = new DGraph();
-////////////////////////////////////////first push to gragh
+		////////////////////////////////////////first push to gragh
 		gg.init(g);
-
-
 
 		String info = game.toString();
 
@@ -112,8 +101,6 @@ public class SimpleGameClient {
 
 			rs = ttt.getInt("robots");//num of robots
 
-
-
 			////////////////////////////////////////////////////////////enter fruit to gragh/////////
 
 			Iterator<String> f_iter = game.getFruits().iterator();
@@ -125,25 +112,16 @@ public class SimpleGameClient {
 
 			}	
 
-
-
-
-
 			///////////////location  robots/////////////////////////////our algorithem begin here
 
-///spread the robots on server gragh
+			///spread the robots on server gragh
 			int pizur= gg.Vertex.size()/rs;
 
 			for(int a = 0;a<rs;a++) {
-
 				game.addRobot((pizur-1)%gg.Vertex.size());
 				pizur+=pizur;
 
 			}
-
-
-
-
 
 			//******************************************connect fruit to edge
 
@@ -180,8 +158,6 @@ public class SimpleGameClient {
 
 								}}
 						}
-
-
 						else continue;
 					}
 				}
@@ -210,23 +186,23 @@ public class SimpleGameClient {
 			Robot ans=new Robot(ro.getInt("id"),ro.getInt("value"),ro.getInt("src"),ro.getInt("dest"),ro.getInt("speed"),ro.getString("pos"));
 			gg.addrobot(ans);
 		}	
-		
+
 		///////first gui show///////////////////////////
 
 		Graph_gui gu = new Graph_gui();
 
 		gu.addGraph(gg);
 
-	   gu.setVisible(true);
+		gu.setVisible(true);
 
 		// should be a Thread!!!
 
-
+		Move m = new Move(game, gg, gu);
 
 		while(game.isRunning()) {
 
 			/////////////////////////////////////where each robot move////////////////
-			moveRobots(game, gg);
+			m.run();
 		}
 
 		////////////////////////////////////////end game/////////////
@@ -238,127 +214,9 @@ public class SimpleGameClient {
 	}
 
 	private static boolean check_on_line(Point3D p, Point3D src, Point3D dest) {
-
 		double e = 0.0000001;
-
-		if(  (src.distance2D(p)+p.distance2D(dest)-src.distance2D(dest)) < e) return true;
-
+		if((src.distance2D(p)+p.distance2D(dest)-src.distance2D(dest)) < e) return true;
 		return false;
-
 	}
-
-	/** 
-
-	 * Moves each of the robots along the edge, 
-
-	 * in case the robot is on a node the next destination (next edge) is chosen (randomly).
-
-	 * @param game
-
-	 * @param gg
-
-	 * @param log
-
-	 */
-
-	private static void moveRobots(game_service game, DGraph gg  ) {
-///move the robots 1 step
-		List<String> log = game.move();
-		if(log!=null) {
-			long t = game.timeToEnd();
-///run on every robot and see if we need to enter new direction to robot
-			for(int i=0;i<log.size();i++) {
-				String robot_json = log.get(i);
-				try {
-					JSONObject line = new JSONObject(robot_json);
-					JSONObject ttt = line.getJSONObject("Robot");
-					int rid = ttt.getInt("id");
-					int src = ttt.getInt("src");
-					int dest = ttt.getInt("dest");
-					String pos=ttt.getString("pos");
-					String[] cord= pos.split(",");
-					Point3D ans=new Point3D(scale(Double.parseDouble(cord[0]),35.186179,35.2142,0,1000),
-							scale(Double.parseDouble(cord[1]),32.100148,32.109347,100,600));
-	
-					if(dest==-1) {//no direcrtion	
-						dest = nextNode(gg, src,rid);///choose the node
-						game.chooseNextEdge(rid, dest);///sent to server
-						System.out.println("Turn to node: "+dest+"  time to end:"+(t/1000));
-						System.out.println(ttt);
-					}
-					gg.Robots.get(rid).pos=	ans;					
-
-				} 
-				catch (JSONException e) {e.printStackTrace();}
-			}
-			gu.addGraph(gg);
-			gu.setVisible(true);
-		}
-	}
-	public static  double scale(double data, double r_min, double r_max, 
-			double t_min, double t_max)
-	{
-
-		double res = ((data - r_min) / (r_max-r_min)) * (t_max - t_min) + t_min;
-		return res;
-	}
-
-	/**
-
-	 *  implementation imanuel algo!
-
-	 * @param g
-
-	 * @param src
-
-	 * @return
-
-	 */
-
-	private static int nextNode(graph g, int src,int rid) {
-////////creat th targets list
-		if (gg.Robots.get(rid).path==null) {
-			double minpath=Integer.MAX_VALUE;///the dist to the fruit
-		for ( Fruit fr : gg.Fruits) {
-			Graph_Algo gr= new Graph_Algo();
-			gr.init(gg);
-			double dist=gr.shortestPathDist(src, fr.ed.getSrc());///the dist to the  current fruit
-			if (fr.withrob==-1&&dist<minpath);////the fruit not cout
-			minpath=dist;
-			gg.Robots.get(rid).dest=fr.ed.getSrc();///the trget of this robot
-
-		}
-		gg.Robots.get(rid).path=gg.getPath(gg.getNode(gg.Robots.get(rid).src), gg.getNode(gg.Robots.get(rid).dest));///the way of this robot
-		return gg.Robots.get(rid).dest;///finish
-
-	}
-////////////the list exist
-		if (gg.Robots.get(rid).path.size()>0){///thers more then 1
-			gg.Robots.get(rid).path.remove(0);
-		
-////////the list is empty after remove
-if (gg.Robots.get(rid).path.isEmpty()) {
-		double minpath=Integer.MAX_VALUE;///the dist to the fruit
-		for ( Fruit fr : gg.Fruits) {
-			Graph_Algo gr= new Graph_Algo();
-			gr.init(gg);
-			double dist=gr.shortestPathDist(src, fr.ed.getSrc());///the dist to the  current fruit
-			if (fr.withrob==-1&&dist<minpath);////the fruit not cout
-			minpath=dist;
-			gg.Robots.get(rid).dest=fr.ed.getSrc();///the trget of this robot
-
-		}
-		gg.Robots.get(rid).path=gg.getPath(gg.getNode(gg.Robots.get(rid).src), gg.getNode(gg.Robots.get(rid).dest));///the way of this robot
-
-	}
-else///the list not empty
-	gg.Robots.get(rid).dest=gg.Robots.get(rid).path.get(0).getKey();
-
-		}
-
-
-return gg.Robots.get(rid).dest;
-	}
-
 
 }
